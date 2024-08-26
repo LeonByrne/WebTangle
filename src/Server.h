5744a3887f0386e7393abde4d1da41f32054a7e0
LeonByrne <leonbyrne52@gmail.com>
2024-08-26 15:24:01 +0100

    Ready to implement mappings inside server

--- src/Server.h
+++ src/Server.h
@@ -7,6 +7,8 @@
 
 #include <arpa/inet.h>
 
+#include "UrlMapping.h"
+
 typedef struct 
 {
   int server_fd;
@@ -14,16 +16,23 @@ typedef struct
 
   bool running;
   int type;
-  // TODO add a hashtable for url strings to function pointers
-  
 
-  pthread_mutex_t socketLock;
+  UrlMapping *mappings;
+  int nMappings;
+
   int numThreads;
   pthread_t *threadPool;
+
+  pthread_mutex_t server_lock;  // Lock to protect the data of the server
+  pthread_mutex_t socketLock;   // Lock to protect the socket
 } Server;
 
 // TODO make more and less specific server_init functions
 int server_init(Server *, const int, const int);
+
+int add_mapping(Server *, UrlMapping *);
+int remove_mapping(Server *, char *);
+UrlMapping * get_mapping(Server *, char *);
 
 int start_server(Server *);
 int pause_server(Server *);
