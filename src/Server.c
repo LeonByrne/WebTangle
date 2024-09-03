80dc28be4cfe705911092dc8e6daca4aced708b1
Leon Byrne <leonbyrne52@gmail.com>
2024-09-03 20:57:39 +0100

    Implemented some more essential functions.

--- src/Server.c
+++ src/Server.c
@@ -16,13 +16,14 @@ typedef struct RequestNode
 static int server_fd;
 static bool running;
 
-static UrlMapping *mappings = NULL;
+static UrlMapping **mappings = NULL;
 static int nMappings = 0;
 
 static pthread_t listenThread;
 static pthread_t *threadPool;
 static int nThreads;
 
+void * listen_thread(void *);
 void * worker_thread(void *);
 
 static pthread_mutex_t queueLock = PTHREAD_MUTEX_INITIALIZER;
@@ -41,21 +42,55 @@ HttpRequest * dequeue_request();
  */
 int WT_init(const int port)
 {
+  // Setup address
+  struct sockaddr_in address;
+  address.sin_family = AF_INET;
+  address.sin_port = htons(port);
+  address.sin_addr.s_addr = INADDR_ANY;
+
   // Create socket
+  server_fd = socket(AF_INET, SOCK_STREAM, 0);
+  if(server_fd == 0)
+  {
+    return -1;
+  }
 
   // Bind socket
+  if(bind(server_fd, (struct sockaddr *) &address, sizeof(address)) != 0)
+  {
+    close(server_fd);
+    return -1;
+  }
+
+  // Set listen
+  // TODO how big should the max queue be for listen?
+  if(listen(server_fd, 32) != 0)
+  {
+    close(server_fd);
+    return -1;
+  }
 
   // Set running to true
+  running = true;
 
+// TODO check if threads were made successfully
   // Create listen thread
+  pthread_create(&listenThread, NULL, listen_thread, NULL);
 
   // Create thread pool
+  nThreads = 4;
+  for(int i = 0; i < nThreads; i++)
+  {
+    pthread_create(&threadPool[i], NULL, worker_thread, NULL);
+  }
 
   return 0;
 }
 
 int WT_shutdown()
 {
+  // TODO implement later
+
   // Set running to false
 
   // Stop listen thread
@@ -69,22 +104,70 @@ int WT_shutdown()
 
 int WT_add_mapping(const char *method, const char *url, void (*handler)(HttpRequest *))
 {
+  // TODO Choose failure codes later
+
   // Create mapping
+  UrlMapping *mapping = create_mapping(method, url, handler);
+
+  if(mapping == NULL)
+  {
+    return -1;
+  }
 
   // Check if mapping already exists
-  // If exists return failure
+  for(int i = 0; i < nMappings; i++)
+  {
+    // TODO account for different methods later
+    if(strcmp(mappings[i]->url, mapping->url) == 0)
+    {
+      // If exists return failure
+      return -1;
+    }
+  }
+
   // Else add it
-
-  // Realloc to get additional space
-  
-  // Add mapping to new space
-
-  // Increase mapping count
+  mappings = realloc(mappings, (nMappings + 1) * sizeof(UrlMapping *));
+  mappings[nMappings] = mapping;
+  nMappings++;
 
   return 0;
 }
 
-void *worker_thread(void *)
+void * listen_thread(void *)
+{
+  // TODO setup pollfd timeout
+
+  while(running)
+  {
+    // TODO pollfd before accepting
+
+    // Accept new connection
+    int client_fd = accept(server_fd, NULL, NULL);
+    if(client_fd < 0)
+    {
+      continue;
+    }
+
+    // Allocate buffer
+    char *buffer = malloc(4096);
+
+    // Read incoming data
+    int bytesRead = read(client_fd, buffer, 4096);
+    if(bytesRead > 0)
+    {
+      HttpRequest *request = create_request(buffer, client_fd);
+      enqueue_request(request);
+    } else
+    {
+      free(buffer);
+    }
+
+    // TODO Should be closed by delete_request function maybe?
+    close(client_fd);
+  }
+}
+
+void * worker_thread(void *)
 {
   while(running)
   {
@@ -98,6 +181,7 @@ void *worker_thread(void *)
     }
 
     // TODO resolve method/url to handler and pass data along
+    printf("url: %s\n", request->url);
 
     delete_request(request);
   }
