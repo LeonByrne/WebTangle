067976ae641c5727fa96c20fe8ae64864c80841f
Leon Byrne <leonbyrne52@gmail.com>
2024-09-27 15:13:24 +0100

    Pages can now have path varibales. Not used serverside, but might be used clientside.

--- src/PageMapping.c
+++ src/PageMapping.c
@@ -3,6 +3,8 @@
 // This will allow strdup to be defined.
 #define _POSIX_C_SOURCE 200809L
 
+#include "Utils.h"
+
 #include <string.h>
 #include <malloc.h>
 #include <unistd.h>
@@ -24,6 +26,15 @@ PageMapping * create_page_mapping(const char *url, const char *filepath)
   this->url = strdup(url);
   this->filepath = strdup(filepath);
 
+  char *regStr = WT_url_to_regex(url);
+  if(regcomp(&this->regex, regStr, REG_EXTENDED) != 0)
+  {
+    delete_page_mapping(this);
+    free(regStr);
+    return NULL;
+  }
+  free(regStr);
+
   return this;
 }
 
@@ -31,6 +42,8 @@ void delete_page_mapping(PageMapping *this)
 {
   free(this->url);
   free(this->filepath);
+
+  regfree(&this->regex);
 
   free(this);
 }
--- src/PageMapping.h
+++ src/PageMapping.h
@@ -1,10 +1,13 @@
 #ifndef WT_PAGE_MAPPING_H
 #define WT_PAGE_MAPPING_H
 
+#include <regex.h>
+
 typedef struct PageMapping
 {
   char *url;
   char *filepath;
+  regex_t regex;
 } PageMapping;
 
 PageMapping * create_page_mapping(const char *url, const char *filepath);
