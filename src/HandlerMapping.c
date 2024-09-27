361b1a1dc16ba86ebf14c25393b709ea16673596
Leon Byrne <leonbyrne52@gmail.com>
2024-09-27 15:12:51 +0100

    See last commit.

--- src/HandlerMapping.c
+++ src/HandlerMapping.c
@@ -1,62 +1,12 @@
 #include "HandlerMapping.h"
 
+// This will allow strdup to be defined.
+#define _POSIX_C_SOURCE 200809L
+
+#include "Utils.h"
+
 #include <malloc.h>
 #include <string.h>
-
-/**
- * @brief Converts a url string into an appropiate string to make a regex
- * 
- * @param url 
- * @return char* 
- */
-char * url_to_regex(const char *url)
-{
-	// No clue why this would need a buffer this big
-	// TODO work out the url to regex max size
-	char buffer[strlen(url) * 2];
-
-	// Go through string, replace path variables and add escape chars
-	int len = strlen(url);
-	int j = 0;
-	buffer[j++] = '^'; // Regex start char
-	for(int i = 0; i < len; i++)
-	{
-		switch(url[i])
-		{
-			case '/':
-			case ':':
-			case '.':
-				// Escape character needed for special characters for regex
-				buffer[j++] = '\\';
-				buffer[j++] = url[i];
-				break;
-			case '{':
-				// Replace path var with [^/]+ to match anything but /
-				while(url[i] != '}' && i < len)
-				{
-					i++;
-				}
-
-				strcpy(&buffer[j], "[^/]+");
-				j+= strlen("[^/]+");
-				break;
-			default:
-				// Copy all other characters
-				buffer[j++] = url[i];
-				break;
-		}
-	}
-
-	buffer[j++] = '$';
-	buffer[j] = '\0';
-
-	// Make string of right size and copy buffer to it
-	char *regex = malloc(j);
-	strcpy(regex, buffer);
-
-	// Return finished regex
-	return regex;
-}
 
 /**
  * @brief Create a mapping object
@@ -71,12 +21,10 @@ HandlerMapping * create_handler_mapping(const char *method, const char *url, voi
 
 	HandlerMapping *this = malloc(sizeof(HandlerMapping));
 
-	// Allocate space and copy url string
-	this->url = malloc(strlen(url) + 1);
-	strcpy(this->url, url);
+	this->url = strdup(url);
 
 	// Convert url into regex string, convert string to regex and free regex string
-	char *regStr = url_to_regex(url);
+	char *regStr = WT_url_to_regex(url);
 	if(regcomp(&this->regex, regStr, REG_EXTENDED) != 0)
 	{
 		delete_handler_mapping(this);
